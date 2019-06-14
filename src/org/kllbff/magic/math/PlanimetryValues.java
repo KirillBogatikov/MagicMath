9f0733b43877a57e37bc7cdc6e65fdee565c87a3
Kirill Bogatikov <kivi15penza@gmail.com>
2019-06-15 01:37:48 +0300

    PlanimatryValues class added
    Class contains some solutions for getting low accuracy sin and cos
    values for specified angles

---
+++ src/org/kllbff/magic/math/PlanimetryValues.java
@@ -0,0 +1,95 @@
+package org.kllbff.magic.math;
+
+/**
+ * Provides methods to calculating some fixed values with relatively low accuracy:
+ * <ul>
+ *     <li>Sine of angle: {@link #sin(double)}</ul>
+ *     <li>Cosine of angle: {@link #cos(double)}</ul>
+ * </ul>
+ * 
+ * Also class contains some previously calculated values, such as
+ * <ul>
+ *     <li>PI / 2 - radian measure of 90 degrees angle;</li>
+ *     <li>PI / 4 - radian measure of 45 degrees angle;</li>
+ *     <li>&#8730;2 - square root from 2;</li>
+ *     <li>&#8730;3 - square root from 3;</li>
+ * </ul> 
+ * 
+ * Accuracy of calculations fixed by value of {@link #MAX_DIGITS_COUNT}, as default it is {@value #MAX_DIGITS_COUNT}} 
+ * 
+ * @author Kirill Bogatikov
+ * @version 1.0
+ * @since 1.0
+ */
+public class PlanimetryValues {
+    public static final double PI2 = Math.PI / 2,
+                               PI4 = Math.PI / 4;
+    public static final double SQRT_OF_TWO   = Math.sqrt(2),
+                               SQRT_OF_THREE = Math.sqrt(3);
+    public static final int MAX_DIGITS_COUNT = 16;
+    
+    /**
+     * Returns a value, rounded to specified number of decimal digits
+     * <p>If specified digits count greater than {@value #MAX_DIGITS_COUNT}, returned initial value</p>
+     * <p>If specified digits count is negative number, returns {@link Double#NEGATIVE_INFINITY} value</p>
+     * 
+     * @param value initial value
+     * @param digits specified number of decimal digit
+     * @return value, rounded to specified number of decimal digits
+     */
+    public static double round(double value, int digits) {
+        if(digits >= MAX_DIGITS_COUNT) {
+            return value;
+        }
+        if(digits < 0) {
+            return Double.NEGATIVE_INFINITY;
+        }
+        if(digits == 0) {
+            return Math.round(value);                    
+        }
+        
+        double multiplexor = Math.pow(10, digits);
+        
+        return Math.round(value * multiplexor) / multiplexor;
+    }
+    
+    /**
+     * Returns sinus of specified angle
+     * <p>Default {@link Math#sin(double)} method can return vary accuracy value, such as <code>6.123031769111886e-17</code> for sin(&#960; / 2).
+     *    This method returns fixed values for basic angles: 0, 90, 180 and 270 degrees. Thus, you no longer need to manually keep track of and rounding the value for the most proper use and display</p>
+     * 
+     * @param angle specified angle, in radians
+     * @return sinus of specified angle
+     */
+    public static double cos(double angle) {
+        int degrees = (int)round(Math.toDegrees(round(angle, 5)), 0);
+        degrees = (degrees + 360) % 360;
+        
+        switch(degrees) {
+            case 0: return 1;
+            case 180: return -1;
+            case 90: case 270: return 0;
+            default: return round(Math.cos(angle), 10);
+        }
+    }
+    
+    /**
+     * Returns cosine of specified angle
+     * <p>Default {@link Math#sin(double)} method can return vary accuracy value, such as <code>-6.123031769111886e-17</code> for cos(&#960;).
+     *    This method returns fixed values for basic angles: 0, 90, 180 and 270 degrees. Thus, you no longer need to manually keep track of and rounding the value for the most proper use and display</p>
+     * 
+     * @param angle specified angle, in radians
+     * @return cosine of specified angle
+     */
+    public static double sin(double angle) {
+        int degrees = (int)round(Math.toDegrees(round(angle, 5)), 0);
+        degrees = (degrees + 360) % 360;
+        
+        switch(degrees) {
+            case 90: return 1;
+            case 270: return -1;
+            case 0: case 180: return 0;
+            default: return round(Math.cos(angle), 10);
+        }
+    }
+}
